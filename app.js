2180dfbb3a731bbb7de4f4f4ff4dc4608839e1b2
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-10 20:45:10 -0700

    finished storing passwords. Working on encrypting the database passwords.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static("public"));
@@ -10,6 +10,18 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
   extended:true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{
+  useNewUrlParser:true
+});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = mongoose.model("User",userSchema);
+
 
 app.get('/',function(req,res){
   res.render("home");
@@ -22,6 +34,70 @@ app.get('/login',function(req,res){
 app.get('/register',function(req,res){
   res.render("register");
 });
+
+
+
+app.post('/register',function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        console.log("User exists.");
+        res.render("home");
+      }else{
+        const newUser = new User({
+          email: username,
+          password: password
+        });
+
+
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+          } else{
+            res.render("secrets");
+          }
+        });
+      }
+    }
+  });
+
+
+});
+
+
+app.post('/login',function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }else{
+        console.log("User not found.");
+        res.render("home");
+      }
+    }
+  });
+
+});
+
+
+
+
+
+
+
+
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
