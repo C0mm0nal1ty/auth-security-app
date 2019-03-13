738c1708905283521022dd98fe64e0fdb1d0ffe8
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-12 20:25:51 -0700

    completed cookies and session. working on integrating 0Auth.

--- app.js
+++ app.js
@@ -39,21 +39,22 @@ mongoose.connect("mongodb://localhost:27017/userDB",{
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
-  email: String,
+  username: String,
   password: String
 });
 
 //create the plugin
-userSchema.plugin(passportLocalMongoose)
+//this will handle the salting and hashing of a users login credentials
+userSchema.plugin(passportLocalMongoose);
 
 // const secret = process.env.SECRET;
 
 //create the strategy
 //serialize a user creates a cookie and stores user information into that cookie
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-// use static authenticate method of model in LocalStrategy
+//
 passport.use(User.createStrategy());
 
 // use static serialize and deserialize of model for passport session support
@@ -73,15 +74,54 @@ app.get('/register',function(req,res){
   res.render("register");
 });
 
+app.get('/secrets',function(req,res){
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }else{
+    res.redirect('/login');
+  }
 
+});
+
+app.get('/logout',function(req,res){
+  req.logout();
+  res.redirect('/');
+});
 
 app.post('/register',function(req,res){
+  User.register({username: req.body.username}, req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect('/register');
+    }
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect('/secrets');
+      });
+    }
+  });
+
 
 });
 
 
 app.post('/login',function(req,res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
+req.login(user,function(err){
+  if(err){
+    console.log(err);
+    res.redirect('login');
+  }
+  else{
+    passport.authenticate('local', {failureRedirect: "/login"})(req,res,function(){
+      res.redirect('/secrets');
+    });
+  }
+});
 
 });
 
@@ -95,4 +135,4 @@ app.post('/login',function(req,res){
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
-})
+});
