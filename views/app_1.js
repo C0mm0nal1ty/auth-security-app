0d2a598502c1f7f52820b24fc6e6de987f7e1e2e
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-12 19:23:19 -0700

    stored previous code in app_1.js

--- views/app_1.js
+++ views/app_1.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+//code used for hashing and salting before I implemented cookies and sessions.
 require('dotenv').config();
 
 const express = require('express');
@@ -12,7 +13,9 @@ const passportLocal = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
 const expressSession = require('express-session');
 
+
 const app = express();
+
 
 const saltRounds = 10;
 
@@ -51,13 +54,59 @@ app.get('/register',function(req,res){
 
 
 app.post('/register',function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        console.log("User exists.");
+        res.render("home");
+      }else{
+        bcrypt.hash(password, saltRounds, function(err, hash){
+
+          const newUser = new User({
+            email: username,
+            password: hash
+          });
+
+          newUser.save(function(err){
+            if(err){
+              console.log(err);
+            } else{
+              res.render("secrets");
+            }
+          });
+        });
+      }
+    }
+  });
 
 });
 
 
 app.post('/login',function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
 
-
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        bcrypt.compare(password,foundUser.password,function(err,result){
+          if(result === true){
+            res.render("secrets");
+          }
+        });
+      } else{
+        console.log("User not found.");
+        res.render("home");
+      }
+    }
+  });
 });
 
 
